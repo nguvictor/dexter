449ad9c0aa5c3526681b0520f1a16e8776256efa
Victor <victor@sentryhealth.ca>
2023-09-25 09:03:19 -0400

    finetuning styles

--- src/app/components/PairSelector.tsx
+++ src/app/components/PairSelector.tsx
@@ -13,18 +13,17 @@ export function PairSelector() {
         className="dropdown dropdown-start w-full not-prose"
         id="pair-selector"
       >
-        <label tabIndex={0} className="btn btn-block m-1 font-bold text-left">
-          {pairSelector.name}
+        <label tabIndex={0} className="btn btn-block m-1 font-bold">
+          <span className="pr-9">{pairSelector.name}</span>
           <span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
-              fill="currentColor"
+              fill="white"
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
                 d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
               />
             </svg>
@@ -34,7 +33,7 @@ export function PairSelector() {
           tabIndex={0}
           className="dropdown-content z-[1] menu shadow bg-neutral rounded-box w-full mt-0 mb-0"
         >
-          <li>Select a pair of tokens:</li>
+          <li className="pl-1">Select a pair of tokens:</li>
           {pairSelector.pairsList.map((pair, index) => (
             <li className="font-bold" key={index}>
               <button onClick={() => selectPair(pair.address)}>
