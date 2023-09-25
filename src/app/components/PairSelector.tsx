6ab0d25986f6e9c26d02eb8d800de294bac3b1da
Victor <victor@sentryhealth.ca>
2023-09-24 23:06:44 -0400

    rough alignment and positioning

--- src/app/components/PairSelector.tsx
+++ src/app/components/PairSelector.tsx
@@ -8,22 +8,42 @@ export function PairSelector() {
     dispatch(selectPairAddress(pairAddress));
   };
   return (
-    <div className="dropdown" id="pair-selector">
-      <label tabIndex={0} className="btn m-1">
-        {pairSelector.name}
-      </label>
-      <ul
-        tabIndex={0}
-        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+    <div>
+      <div
+        className="dropdown dropdown-start w-full not-prose"
+        id="pair-selector"
       >
-        {pairSelector.pairsList.map((pair, index) => (
-          <li key={index}>
-            <button onClick={() => selectPair(pair.address)}>
-              {pair.name}
-            </button>
-          </li>
-        ))}
-      </ul>
+        <label tabIndex={0} className="btn btn-block m-1 font-bold text-left">
+          {pairSelector.name}
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </span>
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu shadow bg-neutral rounded-box w-full mt-0 mb-0"
+        >
+          <li>Select a pair of tokens:</li>
+          {pairSelector.pairsList.map((pair, index) => (
+            <li className="font-bold" key={index}>
+              <button onClick={() => selectPair(pair.address)}>
+                {pair.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
